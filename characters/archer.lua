977e71e7b1b521eb085ede5aa4a204c3e69c15c7
memoryleak47 <memoryleak47@protonmail.ch>
2017-12-05 13:35:35 +0100

    added arrow cooldown

--- characters/archer.lua
+++ characters/archer.lua
@@ -1,26 +1,34 @@
 return {
 	new = function(player)
 
+		player.arrow_cd = 0
+
 		function player:shoot_arrow(x, y)
-			local arrow = {}
-			arrow.body = love.physics.newBody(world, x, y, "dynamic")
-			arrow.shape = love.physics.newRectangleShape(TILESIZE * 2/3, 5)
-			arrow.fixture = love.physics.newFixture(arrow.body, arrow.shape)
+			if player.arrow_cd == 0 then
+				player.arrow_cd = 1
 
-			function arrow:update(dt) end
+				local arrow = {}
+				arrow.body = love.physics.newBody(world, x, y, "dynamic")
+				arrow.shape = love.physics.newRectangleShape(TILESIZE * 2/3, 5)
+				arrow.fixture = love.physics.newFixture(arrow.body, arrow.shape)
 
-			function arrow:render()
-				love.graphics.setColor(100, 100, 0)
-				love.graphics.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
+				function arrow:update(dt) end
+
+				function arrow:render()
+					love.graphics.setColor(100, 100, 0)
+					love.graphics.polygon("fill", self.body:getWorldPoints(self.shape:getPoints()))
+				end
+
+				entities[#entities + 1] = arrow
 			end
-
-			entities[#entities + 1] = arrow
 		end
 
 		function player:update_character(dt)
 			if love.keyboard.isDown('q') then
 				player:shoot_arrow(player.body:getPosition())
 			end
+
+			player.arrow_cd = math.max(0, player.arrow_cd - dt)
 		end
 
 		return player
